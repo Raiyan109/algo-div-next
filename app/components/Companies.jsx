391501eb3f48c8661254dcf520ef42e145d490cf
Raiyan109 <kraiyan109@gmail.com>
2024-01-30 13:02:08 +0600

    companies

---
+++ app/components/Companies.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import comp1 from '../../public/unsplash.png'
+import comp2 from '../../public/notion.png'
+import comp3 from '../../public/intercom.png'
+import comp4 from '../../public/descript.png'
+import comp5 from '../../public/grammerly.png'
+import Image from 'next/image';
+
+
+const Companies = () => {
+    return (
+        <div className='flex justify-center items-center pb-20'>
+            <div className='space-y-10'>
+                <h1 className="font-bold text-3xl text-center">More than 25,000 teams use Collabs</h1>
+
+                <div className='flex items-center gap-10'>
+                    <Image src={comp1} width={150} />
+                    <Image src={comp2} width={150} />
+                    <Image src={comp3} width={150} />
+                    <Image src={comp4} width={150} />
+                    <Image src={comp5} width={150} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Companies;
--- app/page.js
+++ app/page.js
@@ -2,13 +2,15 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import bg from '../public/green-bh.jpg'
 import Hero from "./components/Hero";
+import Companies from "./components/Companies";
 
 export default function Home() {
   return (
     <div>
-      <div className="bg-no-repeat bg-cover h-screen" style={{ backgroundImage: `url('${bg.src}')` }}>
+      <div className="bg-no-repeat bg-cover" style={{ backgroundImage: `url('${bg.src}')` }}>
         <Navbar />
         <Hero />
+        <Companies />
       </div>
     </div>
   );
